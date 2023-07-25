85c7627fff2dec4399e764cd858a28c37e8de4c9
acdc1111 <134829713+acdc1111@users.noreply.github.com>
2023-07-25 17:21:54 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    $('.switch').click(function (e) {
+        e.preventDefault();
+        $('.switch').toggleClass('active')
+        $('body').toggleClass('dark')
+    });
+
+    // slider
+    $(".portfolios-slider").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+    })
+
+    $(".btn-prev").click(function(){
+        $(".portfolios-slider").slick("slickPrev")  
+    })
+
+    $(".btn-next").click(function(){
+        $(".portfolios-slider").slick("slickNext")  
+    })
+
+    $(".slider-btn").click(function (e) { 
+        e.preventDefault();
+        $(".slider-btn").removeClass("active")
+        $(this).addClass("active")
+    });
+
+    // tab items
+
+    $(".experices-list").on("click", ".experices-item:not(.active)", function(){
+        $(this).addClass("active")
+            .siblings()
+            .removeClass("active")
+            .closest('.experices-wrapper')
+            .find('.experisec-tabs .tab')
+            .removeClass('active')
+            .eq($(this).index())
+            .addClass('active')
+    })
+});
